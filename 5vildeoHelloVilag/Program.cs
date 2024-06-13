2f601d27d117bb7b88cbc442e0a4bed473147705
Tóth Károly <toth.karolycar84@gmail.com>
2024-06-13 21:10:45 +0200

    Add project files.

---
+++ 5vildeoHelloVilag/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _5vildeoHelloVilag
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello világ!");
+        }
+    }
+}
